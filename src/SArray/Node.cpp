33d27ee6587aba914c608a5c17aea85d2ee5d20e
Nick Bury <nick@debian.(none)>
2012-10-25 16:21:29 -0400

    added Node class

---
+++ src/SArray/Node.cpp
@@ -0,0 +1,45 @@
+#include "Node.h"
+#include <string>
+
+template <typename T>
+Node<T>::Node(int r, int c, T v) {
+  row = r;
+  col = c;
+  value = v;
+}
+template <typename T>
+Node<T>::~Node(){
+
+}
+template <typename T>
+T Node<T>::getValue() {
+  return value;
+}
+template <typename T>
+int Node<T>::getRow() {
+  return row;
+}
+template <typename T>
+int Node<T>::getCol() {
+  return col;
+}
+template <typename T>
+void Node<T>::setNextDown(Node<T>* nd) {
+  down = nd;
+}
+template <typename T>
+void Node<T>::setNextRight(Node<T>* nr) {
+  right = nr;
+}
+template <typename T>
+Node<T>* Node<T>::getNextDown() {
+  return down;
+}
+template <typename T>
+Node<T>* Node<T>::getNextRight() {
+  return right;
+}
+
+template class Node<int>;
+template class Node<double>;
+template class Node<std::string>;
---
+++ src/SArray/Node.h
@@ -0,0 +1,25 @@
+#ifndef __NODE_H__
+#define __NODE_H__
+
+template <typename T>
+class Node {
+  private:
+    T value;
+    int row;
+    int col;
+    Node<T>* down;
+    Node<T>* right;
+    
+  public:
+    Node<T>(int r, int c, T v);
+    ~Node<T>();
+    T getValue();
+    int getRow();
+    int getCol();
+    void setNextDown(Node<T>* nd);
+    void setNextRight(Node<T>* nr);
+    Node<T>* getNextDown():
+    Node<T>* getNextRight():
+};
+
+#endif
